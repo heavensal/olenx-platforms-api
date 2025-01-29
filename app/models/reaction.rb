5a13558d4524e3c33c12fcacd5cdb1ee31fd64a5
Adam Lamouri <adamlamouri@1344.fr>
2025-01-29 01:30:22 +0100

    correct

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -2,7 +2,7 @@ class Reaction < ApplicationRecord
   belongs_to :user
   belongs_to :idea
 
-  validates reaction_type, presence: true
   validates :user_id, uniqueness: { scope: :idea_id }
+  validates :reaction_type, presence: true
   validates :reaction_type, inclusion: { in: %w[like dislike] }
 end
